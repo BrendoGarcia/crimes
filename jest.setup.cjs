6d766f3a41c69321e95db26c290d07c0a8bdd20d
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2025-12-05 02:36:21 -0300

    Update jest.setup.cjs

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,1 +1,10 @@
 require('@testing-library/jest-dom');
+
+// Mock do ResizeObserver usado pelo Recharts
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.ResizeObserver = ResizeObserver;
